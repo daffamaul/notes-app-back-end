64b00877b94dfdcaed1bd99b41688dc3c10df106
daffamaul <dffmln2004@gmail.com>
2023-12-19 14:16:09 +0700

    postgres database implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,27 +1,27 @@
-const ClientError = require("../../exceptions/ClientError");
+const ClientError = require('../../exceptions/ClientError');
 
 class NotesHandler {
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;
 
-    // this.postNoteHandler = this.postNoteHandler.bind(this);
-    // this.getAllNotesHandler = this.getAllNotesHandler.bind(this);
-    // this.getNoteByIdHandler = this.getNoteByIdHandler.bind(this);
-    // this.putNoteByIdHandler = this.putNoteByIdHandler.bind(this);
-    // this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
+    this.postNoteHandler = this.postNoteHandler.bind(this);
+    this.getAllNotesHandler = this.getAllNotesHandler.bind(this);
+    this.getNoteByIdHandler = this.getNoteByIdHandler.bind(this);
+    this.putNoteByIdHandler = this.putNoteByIdHandler.bind(this);
+    this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
   }
 
-  postNoteHandler(request, h) {
+  async postNoteHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
-      const { title = "untitle", tags, body } = request.payload;
+      const { title = 'untitle', tags, body } = request.payload;
 
-      const noteId = this._service.addNote({ title, tags, body });
+      const noteId = await this._service.addNote({ title, tags, body });
 
       const response = h.response({
-        status: "success",
-        message: "Catatan berhasil ditambahkan",
+        status: 'success',
+        message: 'Catatan berhasil ditambahkan',
         data: {
           noteId,
         },
@@ -31,15 +31,15 @@ class NotesHandler {
     } catch (error) {
       if (error instanceof ClientError) {
         const response = h.response({
-          status: "fail",
+          status: 'fail',
           message: error.message,
         });
         response.code(error.statusCode);
         return response;
       }
       const response = h.response({
-        status: "error",
-        message: "Maaf, terjadi kegagalan pada server kami.",
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
       });
       response.code(500);
       console.error(error);
@@ -47,22 +47,22 @@ class NotesHandler {
     }
   }
 
-  getAllNotesHandler() {
-    const notes = this._service.getNotes();
+  async getAllNotesHandler() {
+    const notes = await this._service.getNotes();
     return {
-      status: "success",
+      status: 'success',
       data: {
         notes,
       },
     };
   }
 
-  getNoteByIdHandler(request, h) {
+  async getNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
-      const note = this._service.getNoteById(id);
+      const note = await this._service.getNoteById(id);
       const response = h.response({
-        status: "success",
+        status: 'success',
         data: {
           note,
         },
@@ -72,15 +72,15 @@ class NotesHandler {
     } catch (error) {
       if (error instanceof ClientError) {
         const response = h.response({
-          status: "fail",
+          status: 'fail',
           message: error.message,
         });
         response.code(error.statusCode);
         return response;
       }
       const response = h.response({
-        status: "error",
-        message: "Maaf, terjadi kegagalan pada server kami.",
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
       });
       response.code(500);
       console.error(error);
@@ -94,21 +94,21 @@ class NotesHandler {
       const { id } = request.params;
       this._service.editNoteById(id, request.payload);
       return {
-        status: "success",
-        message: "Catatan berhasil diperbarui",
+        status: 'success',
+        message: 'Catatan berhasil diperbarui',
       };
     } catch (error) {
       if (error instanceof ClientError) {
         const response = h.response({
-          status: "fail",
+          status: 'fail',
           message: error.message,
         });
         response.code(error.statusCode);
         return response;
       }
       const response = h.response({
-        status: "error",
-        message: "Maaf, terjadi kegagalan pada server kami.",
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
       });
       response.code(500);
       console.error(error);
@@ -121,21 +121,21 @@ class NotesHandler {
       const { id } = request.params;
       this._service.deleteNoteById(id);
       return {
-        status: "success",
-        message: "Catatan berhasil dihapus",
+        status: 'success',
+        message: 'Catatan berhasil dihapus',
       };
     } catch (error) {
       if (error instanceof ClientError) {
         const response = h.response({
-          status: "fail",
+          status: 'fail',
           message: error.message,
         });
         response.code(error.statusCode);
         return response;
       }
       const response = h.response({
-        status: "error",
-        message: "Maaf, terjadi kegagalan pada server kami.",
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
       });
       response.code(500);
       console.error(error);
--- src/api/notes/routes.js
+++ src/api/notes/routes.js
@@ -1,28 +1,33 @@
 const routes = (handler) => [
   {
-    method: "POST",
-    path: "/notes",
-    handler: (request, h) => handler.postNoteHandler(request, h),
+    method: 'POST',
+    path: '/notes',
+    // handler: (request, h) => handler.postNoteHandler(request, h),
+    handler: handler.postNoteHandler
   },
   {
-    method: "GET",
-    path: "/notes",
-    handler: () => handler.getAllNotesHandler(),
+    method: 'GET',
+    path: '/notes',
+    // handler: () => handler.getAllNotesHandler(),
+    handler: handler.getAllNotesHandler
   },
   {
-    method: "GET",
-    path: "/notes/{id}",
-    handler: (request, h) => handler.getNoteByIdHandler(request, h),
+    method: 'GET',
+    path: '/notes/{id}',
+    // handler: (request, h) => handler.getNoteByIdHandler(request, h),
+    handler: handler.getNoteByIdHandler
   },
   {
-    method: "PUT",
-    path: "/notes/{id}",
-    handler: (request, h) => handler.putNoteByIdHandler(request, h),
+    method: 'PUT',
+    path: '/notes/{id}',
+    // handler: (request, h) => handler.putNoteByIdHandler(request, h),
+    handler: handler.putNoteByIdHandler
   },
   {
-    method: "DELETE",
-    path: "/notes/{id}",
-    handler: (request, h) => handler.deleteNoteByIdHandler(request, h),
+    method: 'DELETE',
+    path: '/notes/{id}',
+    // handler: (request, h) => handler.deleteNoteByIdHandler(request, h),
+    handler: handler.deleteNoteByIdHandler
   },
 ];
 
